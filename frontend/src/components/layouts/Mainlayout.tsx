190255c89bcec504455097620a30f2e4d660ba5a
JustTaiCorn <taicorn2004@gmail.com>
2026-02-25 15:53:27 +0700

    Continue reffacterr

--- frontend/src/components/layouts/Mainlayout.tsx
+++ frontend/src/components/layouts/Mainlayout.tsx
@@ -5,7 +5,6 @@ import BottomNav from "../common/BottomNav";
 export const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* NavBar hidden on mobile - mobile uses BottomNav instead */}
       <div className="flex-shrink-0 hidden md:block">
         <NavBar />
       </div>
