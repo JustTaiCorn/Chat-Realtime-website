a21e23838d007b157fe94a8c72c2d0400a36caa0
JustTaiCorn <taicorn2004@gmail.com>
2026-02-25 15:52:54 +0700

    hmm

--- frontend/src/components/layouts/Mainlayout.tsx
+++ frontend/src/components/layouts/Mainlayout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../common/NavBar";
+import BottomNav from "../common/BottomNav";
 
 export const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-shrink-0">
+      {/* NavBar hidden on mobile - mobile uses BottomNav instead */}
+      <div className="flex-shrink-0 hidden md:block">
         <NavBar />
       </div>
-      <div className="flex-1 min-h-screen">
+      <div className="flex-1">
         <div className="h-full overflow-y-auto">
           <Outlet />
         </div>
       </div>
+      <BottomNav />
     </div>
   );
 };
